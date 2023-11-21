d822ffca89833575225b9e528ee4af95e4130f6b
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2023-11-21 13:50:31 +0100

    change milestone 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,72 +8,58 @@ let teamMembers = [
     {
         name: "Wayne Barnett",
         role: "Founder & CEO",
-        image: "wayne-barnett-founder-ceo.jpg"
+        image: "./assets/img/wayne-barnett-founder-ceo.jpg"
     },
 
     {
         name: "Angela Caroll",
         role: "Chief Editor",
-        image: "angela-caroll-chief-editor.jpg"
+        image: './assets/img/angela-caroll-chief-editor.jpg'
     },
 
     {
         name: "Walter Gordon",
         role: "Office Manager",
-        image: "walter-gordon-office-manager.jpg"
+        image: './assets/img/walter-gordon-office-manager.jpg'
     },
 
     {
         name: "Angela Lopez",
         role: "Social Media Manager",
-        image: "angela-lopez-social-media-manager.jpg"
+        image: './assets/img/angela-lopez-social-media-manager.jpg'
     },
 
     {
         name: "Scott Estrada",
         role: "Developer",
-        image: "scott-estrada-developer.jpg"
+        image: './assets/img/scott-estrada-developer.jpg'
     },
 
     {
         name: "Barbara Ramos",
         role: "Graphic Designer",
-        image: "barbara-ramos-graphic-designer.jpg"
+        image: './assets/img/barbara-ramos-graphic-designer.jpg'
     }
 ]
+
+console.log(teamMembers);
 
 /*
 MILESTONE 1:
 Stampare su console le informazioni di nome, ruolo e la stringa della foto 
 */
 
-// Uso una funzione per stampare l'array di oggetti
-printArrayMembers(teamMembers)
-
-/* ---------------- FUNCTIONS ---------------- */
-// Funzione che permette di stampare il singolo oggetto in console
-function printObject(object) {
-    let string = "";
-
-    string += "{\n";
-
-    for (let key in object) {
-        string += "\t " + key + ": " + object[key] + "\n";
-    }
-
-    string += "}";
-
-    console.log(string);
-
-}
-
-// Funzione che permette di stampare un array di oggetti
-function printArrayMembers(arrayObjects) {
-    for (let i = 0; i < arrayObjects.length; i++) {
-
-        let member = arrayObjects[i];
-
-        console.log("Team Member n° " + (i + 1) + " :");
-        printObject(member);
+// stampo in console le informazioni di nome, ruolo e stringa della foto
+for (let i = 0; i < teamMembers.length; i++) {
+    let member = teamMembers[i];
+    console.log(`<----- Member ${i + 1} ----->`)
+    for (let key in member) {
+        let membersDetails = member[key];
+        console.log(`${key}: ${membersDetails}`);
     }
 }
+
+/* 
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+*/
