47249d009ff6cd285a69e2532f5480c504a43f1f
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2023-11-21 13:18:14 +0100

    milestone 1 - done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,8 @@
+/* 
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite nella tabella
+*/
+
 // Array di oggetti con i dati dei membri del team
 let teamMembers = [
     {
@@ -36,3 +41,39 @@ let teamMembers = [
         image: "barbara-ramos-graphic-designer.jpg"
     }
 ]
+
+/*
+MILESTONE 1:
+Stampare su console le informazioni di nome, ruolo e la stringa della foto 
+*/
+
+// Uso una funzione per stampare l'array di oggetti
+printArrayMembers(teamMembers)
+
+/* ---------------- FUNCTIONS ---------------- */
+// Funzione che permette di stampare il singolo oggetto in console
+function printObject(object) {
+    let string = "";
+
+    string += "{\n";
+
+    for (let key in object) {
+        string += "\t " + key + ": " + object[key] + "\n";
+    }
+
+    string += "}";
+
+    console.log(string);
+
+}
+
+// Funzione che permette di stampare un array di oggetti
+function printArrayMembers(arrayObjects) {
+    for (let i = 0; i < arrayObjects.length; i++) {
+
+        let member = arrayObjects[i];
+
+        console.log("Team Member n° " + (i + 1) + " :");
+        printObject(member);
+    }
+}
